41a6d1423bbc5d56f51a6b552ac93b7f5fd8a4d0
Austin <austin@unstack.com>
2021-06-26 17:36:06 -0500

    fix: update sorting method

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,6 +4,7 @@ export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   options: {
     storySort: {
+      method: 'alphabetical',
       order: ['Base', 'Components', 'Patterns', 'Templates'],
     },
   },

a389d6853d34d2cf46bfc7bd1a4ad3e3579512ad
Austin <austin@unstack.com>
2021-07-10 12:01:10 -0500

    fix: update profile templates

--- src/stories/Components/Profile/Profile.stories.tsx
+++ src/stories/Components/Profile/Profile.stories.tsx
@@ -12,14 +12,8 @@ export default {
   component: Profile,
   argTypes: {
     variant: {
-      options: ['small', 'medium', 'large'],
+      options: ['small', 'medium', 'large', 'extralarge'],
       control: { type: 'radio' },
-    },
-    color: {
-      control: { type: 'color' },
-    },
-    disabled: {
-      control: { type: 'boolean' },
     },
   },
 } as Meta;
@@ -29,27 +23,23 @@ const Template = (args) => <Profile {...args} />;
 export const Small = Template.bind({});
 Small.args = {
   variant: 'small',
-  disabled: false,
   children: <img src={ProfilePicture} />,
 };
 
 export const Medium = Template.bind({});
 Medium.args = {
   variant: 'medium',
-  disabled: false,
   children: <img src={ProfilePicture} />,
 };
 
 export const Large = Template.bind({});
 Large.args = {
   variant: 'large',
-  disabled: false,
   children: <img src={ProfilePicture} />,
 };
 
 export const ExtraLarge = Template.bind({});
 ExtraLarge.args = {
   variant: 'extralarge',
-  disabled: false,
   children: <img src={ProfilePicture} />,
 };
